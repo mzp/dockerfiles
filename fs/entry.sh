2d59382c7321044db18e78ffdef417745951c6bf
root <root@docker>
2014-08-29 17:42:34 +0900

    add user mod

--- fs/entry.sh
+++ fs/entry.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 service smb start
 service nmb start
+
+for i in $(cat /root/userlist); do
+  user=${i%:*}
+  pass=${i#*:}
+  echo $user
+  useradd $user
+  echo -e "$pass\n$pass" | pdbedit -a $user -t
+done
+
 tail -f /var/log/samba/log.smbd 
